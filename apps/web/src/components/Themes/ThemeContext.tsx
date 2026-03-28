4029bf9a05d59968eddc45521c48e1a2ded3649c
123Qn <quanhuynhvt2004@gmail.com>
2026-03-28 20:28:59 +1100

    darkMode finished 100% pass

--- apps/web/src/components/Themes/ThemeContext.tsx
+++ apps/web/src/components/Themes/ThemeContext.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { createContext } from "react";
+import { Children, createContext, useContext, useEffect, useState } from "react";
 
 export type Theme = "light" | "dark";
 
@@ -13,5 +13,29 @@ const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
 
 // TODOS:
 // 1. Create Theme Provider
+
+export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+    const [theme, setTheme] = useState<Theme>('light');
+
+    const toggleTheme = () => {
+        setTheme(theme === 'light' ? 'dark' : 'light');
+    };
+   useEffect(() => {
+     document.documentElement.setAttribute("data-theme", theme);
+    }, [theme]);
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
 // 2. Create useTheme hook
+
+export const useTheme = () => {
+    const context = useContext(ThemeContext);
+    if (!context) {
+        throw new Error("useTheme must be used within ThemeProvider");
+    }
+    return context;
+};
 // 3. Use the provider in your layout
