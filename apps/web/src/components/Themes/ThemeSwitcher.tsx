fe1cf7b382e9406f60543fa727629290fef0b4e6
123Qn <quanhuynhvt2004@gmail.com>
2026-03-28 21:20:09 +1100

    modify global.css and themeswith.tsx to add icon to the switch mode button

--- apps/web/src/components/Themes/ThemeSwitcher.tsx
+++ apps/web/src/components/Themes/ThemeSwitcher.tsx
@@ -7,8 +7,13 @@ const ThemeSwitch = () => {
   //const theme = "light"; // <- TODO: Get the theme from the context
 const{ theme, toggleTheme } = useTheme();
   return (
-   <Button onClick={toggleTheme}>
-  {theme === "light" ? "Dark Mode" : "Light Mode"}
+<Button onClick={toggleTheme}>
+  <span>
+    {theme === "light" ? "🌙" : "☀️"}
+  </span>
+  <span style={{ marginLeft: "6px" }}>
+    {theme === "light" ? "Dark Mode" : "Light Mode"}
+  </span>
 </Button>
   );
 };
