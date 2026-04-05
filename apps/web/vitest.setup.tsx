6e50b856c6345ecf58faba9e2d43e5aa7f8fabb3
123Qn <quanhuynhvt2004@gmail.com>
2026-04-05 16:53:31 +1000

    admin page done hopeso:((

--- apps/web/vitest.setup.tsx
+++ apps/web/vitest.setup.tsx
@@ -1,1 +1,10 @@
 import "./src/app/globals.css";
+import { vi } from "vitest";
+
+vi.mock("next/link", () => {
+  return {
+    default: ({ children, href }: any) => {
+      return <a href={href}>{children}</a>;
+    },
+  };
+});
