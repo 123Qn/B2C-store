ae7bde2e911264d8dd19fd38f53bdc56c4d3db87
123Qn <quanhuynhvt2004@gmail.com>
2026-04-25 17:33:53 +1000

    Modify the app/page.tsx to fetch db from prisma and blogList to let it appear correctly

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,12 +1,25 @@
-import { posts } from "@repo/db/data";
+import { client } from "@repo/db/client";
 import { AppLayout } from "../components/Layout/AppLayout";
 import { Main } from "../components/Main";
 import styles from "./page.module.css";
-export default function Home() {
-  const activePosts = posts.filter(p => p.active);
+
+export default async function Home() {
+  const prisma = client.db;
+
+  const rawPosts = await prisma.post.findMany({
+    where: { active: true },
+    orderBy: { date: "desc" },
+    include: { Likes: true }
+  });
+
+  const posts = rawPosts.map(p => ({
+    ...p,
+    likes: p.Likes.length
+  }));
+
   return (
     <AppLayout>
-      <Main posts={activePosts} className={styles.main} />
+      <Main posts={posts} className={styles.main} />
     </AppLayout>
   );
 }
