173ea2bd2f56a931060f0a3734a65a70ae7cffb3
123Qn <quanhuynhvt2004@gmail.com>
2026-06-02 17:50:54 +1000

    fix login bug

--- apps/Back-End/next.config.ts
+++ apps/Back-End/next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
       {
         source: '/api/:path*',
         headers: [
-          { key: 'Access-Control-Allow-Origin', value: 'http://localhost:3001' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
           { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE,OPTIONS,PATCH' },
           { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
